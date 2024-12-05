9a803a9a5c40adb4154f75a6f8288de9d7cf49f9
Abhishek Bansal <abhishekbansal2312@gmail.com>
2024-12-05 20:35:29 +0530

    removed unwanted imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Jobs from "./pages/Jobs";
 import Navbar from "./components/Navbar";
 import { JobsProvider } from "./context/JobsProvider";
